6b4b65e016bc1007c9ae8fb44cddccbcfe61c558
mohammed shousha <mshousha29@gmail.com>
2023-08-10 00:18:45 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def print_args(argv):
+	n = len(argv) - 1
+
+	if n == 0:
+		print('{} arguments.'.format(n))
+	elif n == 1:
+		print('{} argument:'.format(n))
+		print('1: {}'.format(argv[1]))
+	else:
+		print('{} arguments:'.format(n))
+		for i in range(1, len(argv)):
+			print('{}: {}'.format(i, argv[i]))
+
+
+if __name__ == '__main__':
+	from sys import argv
+	print_args(argv)
