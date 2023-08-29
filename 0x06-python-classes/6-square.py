f0050397fae81edd9ae895dd1757605279606043
mohammed shousha <mshousha29@gmail.com>
2023-08-29 06:32:00 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,17 +12,14 @@ class Square:
     """
     A square class
 
-    Attributes:
-        __size: private instance size
-
     Args:
-        __size: the size of the square.
-        __position: the position of the square.
+        size: the size of the square.
+        position: the position of the square.
     """
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """Return the current area of the square."""
