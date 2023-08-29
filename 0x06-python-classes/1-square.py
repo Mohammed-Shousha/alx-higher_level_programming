3136904064f32d02eb55c7a1451d33668bfe7ed9
mohammed shousha <mshousha29@gmail.com>
2023-08-29 05:42:04 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module Square
+
+Classes:
+    Square - A square class
+"""
+
+
+class Square:
+    """
+    A square class
+
+    Attributes:
+        __size: private instance size
+
+    Args:
+        __size: the size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
