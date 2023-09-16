a6266a2dbb8d33a79ef848174e46250b5f12fd10
mohammed shousha <mshousha29@gmail.com>
2023-09-16 12:39:37 -0700

    10.Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+"""Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class representation"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """init the square instance"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, n_size):
+        """setter for size"""
+        self.width = n_size
+        self.height = n_size
+
+    def __str__(self):
+        """return the string representation of the square instance"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
