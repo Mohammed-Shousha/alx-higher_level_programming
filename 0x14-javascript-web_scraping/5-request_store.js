7a499e92313ebe56e07ea9a5fec3255b53612bcc
mohammed shousha <mshousha29@gmail.com>
2024-01-03 04:40:16 -0800

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Gets the contents of a webpage and stores it in a file
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url).pipe(fs.createWriteStream(filePath));
