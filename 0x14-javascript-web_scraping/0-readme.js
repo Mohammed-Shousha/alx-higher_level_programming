6f03fe9b34489848a0a9d1f2bda97dc848aaa654
mohammed shousha <mshousha29@gmail.com>
2024-01-03 04:24:49 -0800

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Reads and prints the content of a file
+
+const fs = require('fs');
+
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (error, content) =>
+  console.log(error || content)
+);
