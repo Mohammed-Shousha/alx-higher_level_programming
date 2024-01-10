f45e2e662a35dd324f56e2d3062ce3a78f664a61
Mohammed Shousha <mshousha29@gmail.com>
2024-01-10 17:58:55 +0200

    9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,7 @@
 // JavaScript script that displays the value of hello
 
-$(() => {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', (data, textStatus) => {
-    if (textStatus === 'success') {
-      $('div#hello').text(data.hello);
-    }
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
   });
 });
