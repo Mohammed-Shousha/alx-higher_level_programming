c6bc3be75e99cebe41e5d6eccd186b2a40693b8a
Mohammed Shousha <mshousha29@gmail.com>
2024-01-10 17:47:07 +0200

    5-script.js

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,5 @@
 // JavaScript script that adds a <li> element to a list
 
-$('#add_item').click(function () {
-    $('ul').append('<li>Item</li>');
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
 });
