9a546623d694034398a9f626a5de0fe09592dec7
Mohammed Shousha <mshousha29@gmail.com>
2024-01-10 18:01:35 +0200

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// JavaScript script that updates the text color of the HTML tag HEADER to red (#FF0000):
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
